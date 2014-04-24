904465d5800ea8512c8c65bc08869a6bfc3de616
Thierry Monney <thierry.monney@elca.ch>
2014-04-24 11:32:26 +0100

    Add script to deploy site

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+if [[ "$UID" -neq "0" ]]; then
+	echo "Please run this script as administrator."
+	exit 1
+fi
+
+su -c "git pull && git reset --hard && git clean -fdx" tmonney
+
+WWW_DIR=/var/www/piccoleau.ch
+LOG_DIR=/var/log/apache2/piccoleau.ch
+
+grunt clean
+grunt
+
+mkdir -p $WWW_DIR
+mkdir -p $LOG_DIR
+
+cp -R bin/* $WWW_DIR
+chown -R www-data:www-data $WWW_DIR
