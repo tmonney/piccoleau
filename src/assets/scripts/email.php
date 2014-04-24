56e00abfe2c1caf9cab032df42e220af5a729ce6
Thierry Monney <thierry.monney@elca.ch>
2014-04-24 16:25:28 +0100

    Fix email sending again

--- src/assets/scripts/email.php
+++ src/assets/scripts/email.php
@@ -2,7 +2,7 @@
 	$to = "steph.pythoud@bluewin.ch";
 	$subject = "Contact sur piccoleau.ch";
 	$data = json_decode(file_get_contents('php://input'), true);
-	$message = data['message'];
+	$message = $data['message'];
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/plain;charset=utf-8" . "\r\n";
 	$headers .= "From: " . $data['name'] . '<' . $data['email'] . ">\r\n";
