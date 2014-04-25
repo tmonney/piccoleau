1347a5cfb4f20ec045123660b16308337c72f52d
Thierry Monney <thierry.monney@elca.ch>
2014-04-25 10:36:46 +0200

    Improve email sending

--- src/assets/scripts/email.php
+++ src/assets/scripts/email.php
@@ -1,11 +1,12 @@
 <?php
-	$to = "steph.pythoud@bluewin.ch";
-	$subject = "Contact sur piccoleau.ch";
-	$data = json_decode(file_get_contents('php://input'), true);
-	$message = $data['message'];
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/plain;charset=utf-8" . "\r\n";
-	$headers .= "From: " . $data['name'] . '<' . $data['email'] . ">\r\n";
+        $to = "steph.pythoud@bluewin.ch";
+        //$to = "thierry.monney@gmail.com";
+        $subject = "Contact sur piccoleau.ch";
+        $data = json_decode(file_get_contents('php://input'), true);
+        $message = $data['message'];
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/plain;charset=utf-8" . "\r\n";
+        $headers .= "From: " . $data['name'] . '<' . $data['email'] . ">\r\n";
 
-	mail($to, $subject, $message, $headers);
+        mail($to, $subject, $message, $headers, "-f contact@piccoleau.ch");
 ?>
