8a3033d48366aa0d1edd5137f56c93ca1a3aae8f
Thierry Monney <thierry.monney@gmail.com>
2014-04-29 09:47:41 +0200

    Add max lenghts and parameter validation

--- src/assets/scripts/email.php
+++ src/assets/scripts/email.php
@@ -3,6 +3,25 @@
         //$to = "thierry.monney@gmail.com";
         $subject = "Contact sur piccoleau.ch";
         $data = json_decode(file_get_contents('php://input'), true);
+
+        if(!$data || !$data['message'] || !$data['name'] || !$data['email']) {
+        	http_response_code(400);
+        	return;
+        }
+
+        if(strlen($data['name']) > 100) {
+            http_response_code(400);
+            return;
+        }
+        if(strlen($data['email']) > 254) {
+            http_response_code(400);
+            return;
+        }
+        if(strlen($data['message']) > 2000) {
+            http_response_code(400);
+            return;
+        }
+
         $message = $data['message'];
         $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/plain;charset=utf-8" . "\r\n";
